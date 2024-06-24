e2fc240006f529fefe33c9d3e03eeaa345af260e
Prathmesh <prathmeshgavande117@gmail.com>
2024-06-24 09:28:49 +0530

    java basics

---
+++ Java Basics/HelloWorld.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); 
+        // Hello World!
+    }
+}
+
+// for compile use javac HelloWorld.java .
+// for run use java HelloWorld.
